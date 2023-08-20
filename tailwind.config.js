cac037f0e96a51ec0259d20238e31f88475b6368
Oscar Estrada <arturostrada2020@gmail.com>
2023-08-20 17:59:00 -0600

    Style: Apply rome rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js, jsx, ts, tsx}",
+	content: [
+		"./index.html",
+		"./src/**/*.{js,jsx,ts,tsx}",
 
-    // path tremor node_modules
-    "./node_modules/@tremor/**/*.{js, jsx, ts, tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+		// path tremor node_modules
+		"./node_modules/@tremor/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
