5e3ede691dcaef9b1965585643687106fd0d72a0
Oscar Estrada <arturostrada2020@gmail.com>
2023-08-20 21:16:53 -0600

    Feat: Add redux provider in main.tsx
    Add redux provider and store on the app entry point "main.tsx"

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 );
